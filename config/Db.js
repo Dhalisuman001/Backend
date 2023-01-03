23be26841aceb417bd3829fb5817ae5ae5156e06
Suman Dhali <dhalisuman001@gmail.com>
2023-01-03 11:29:55 +0530

    dotenv Added

--- config/Db.js
+++ config/Db.js
@@ -1,15 +1,12 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const DbConnection = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URl ||
-        "mongodb+srv://Suman:Suman001@cluster0.pcpvei3.mongodb.net/?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
     console.log("Db connected successfully");
   } catch (error) {
     console.log(error);
